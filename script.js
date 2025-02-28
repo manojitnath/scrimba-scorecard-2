003a7f9f93df7da593928962ac8f6a90eb85da48
manojit <163284813+manojitnath@users.noreply.github.com>
2025-02-28 17:49:12 +0530

    final version

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 let homeScoreEl = document.getElementById("homescore");
 let awayScoreEl = document.getElementById("awayscore");
-let resultEl = document.getElementById("result-info");
+let resultEl = document.getElementById("resultinfo");
 
 let homeTeamScore = 0;
 let awayTeamScore = 0;
-let result = "Lets Go!";
+
+function compareResult() {
+  if (homeTeamScore > awayTeamScore) {
+    resultEl.innerText = "Man UTD Leading!";
+  } else if (homeTeamScore < awayTeamScore) {
+    resultEl.innerText = "Liverpool Leading!";
+  } else if ((homeTeamScore === 0, awayTeamScore === 0)) {
+    resultEl.innerText = "Let's GO!";
+  } else {
+    resultEl.innerText = "Its a Draw!";
+  }
+}
 
 function scoreBtnOne() {
   homeTeamScore++;
   homeScoreEl.innerText = homeTeamScore;
+  compareResult();
 }
 
 function scoreBtnTwo() {
   homeTeamScore += 2;
   homeScoreEl.innerText = homeTeamScore;
+  compareResult();
 }
 
 function scoreBtnThree() {
   homeTeamScore += 3;
   homeScoreEl.innerText = homeTeamScore;
+  compareResult();
 }
 
 function scoreBtnFour() {
   awayTeamScore++;
   awayScoreEl.innerText = awayTeamScore;
+  compareResult();
 }
 
 function scoreBtnFive() {
   awayTeamScore += 2;
   awayScoreEl.innerText = awayTeamScore;
+  compareResult();
 }
 
 function scoreBtnSix() {
   awayTeamScore += 3;
   awayScoreEl.innerText = awayTeamScore;
+  compareResult();
 }
 
 function resetBtn() {
@@ -41,4 +58,5 @@ function resetBtn() {
   awayTeamScore = 0;
   homeScoreEl.innerText = homeTeamScore;
   awayScoreEl.innerText = awayTeamScore;
+  compareResult();
 }
